5c09679265d41babae7790e767e9e2942351b052
Tara Ellicott <taraellicott@gmail.com>
2018-01-21 21:02:58 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,26 @@
+function forLoop(array) {
+  for (var i = 0; i < 24; i++) {
+; if (i = 0) {
+  console.log("I am 1 strange loop.");
+} else {
+  console.log(`I am ${i} strange loops.`);
+}
+}
+break;
+}
+
+
+function whileLoop(number) {
+  while (number > 0) {
+    console.log(number);
+  }
+  return 'Done.';
+  break;
+}
+
+
+// function doWhileLoop(array) {
+//   do {
+    
+//   }
+// } while (array.length > 0 && maybeTrue());
